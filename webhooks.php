a476ecb7952d29a899831ea157a77c4d67b80801
Tanawat <minotacs@gmail.com>
2018-10-10 13:39:26 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,7 +23,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $events
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -44,7 +44,7 @@ if (!is_null($events['events'])) {
 			$result = curl_exec($ch);
 			curl_close($ch);
 
-			echo $events . "\r\n";
+			echo $result . "\r\n";
 		}
 	}
 }
