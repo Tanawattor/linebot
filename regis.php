e76588a39152a8ad4dd9c85cec601b36eca696ac
Tanawat <minotacs@gmail.com>
2018-10-11 10:08:37 +0700

    Update regis.php

--- regis.php
+++ regis.php
@@ -1,5 +1,14 @@
 Regis
-<br>
-id = <?php echo $_GET["id"]; ?>
-<br>
-<a href="#" onclick="close_window();return false;">close</a>
+<?php
+
+if(isset($_SESSION['ses_login_userData_val']) && $_SESSION['ses_login_userData_val']!=""){
+    // GET USER DATA FROM ID TOKEN
+    $lineUserData = json_decode($_SESSION['ses_login_userData_val'],true);
+    print_r($lineUserData); 
+    echo "<hr>";
+    echo "Line UserID: ".$lineUserData['sub']."<br>";
+    echo "Line Display Name: ".$lineUserData['name']."<br>";
+    echo '<img style="width:100px;" src="'.$lineUserData['picture'].'" /><br>';
+}
+
+?>
